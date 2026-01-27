508e86f5d2e3091031863c7a5d78c668ec90d1e6
MohammadAlkilidar <mhalkilidar@gmail.com>
2026-01-27 02:42:10 -0600

    Create my_testbench_pkg.svh package with includes

---
+++ my_testbench_pkg.svh
@@ -0,0 +1,11 @@
+package my_testbench_pkg;
+  import uvm_pkg::*;
+
+  `include "my_transaction.svh"
+  `include "my_sequence.svh"
+  `include "my_driver.svh"
+  `include "my_agent.svh"
+  `include "my_env.svh"
+  `include "my_test.svh"
+
+endpackage : my_testbench_pkg
